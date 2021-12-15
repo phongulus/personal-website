102e4466cdd2c70acdd5efcc0d7941a969afa99a
Phong Le <phongnguyen.le22@gmail.com>
2021-12-15 15:40:21 +0800

    Changed border colors

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -10,8 +10,10 @@ function Contact(props) {
         <h1>Get in touch</h1>
         <p>I'm open to internship opportunities right now!</p>
         <p>
-          Please find my resume
-          and contact details below. If you want to collaborate on
+          Please find my resume and contact details below.
+        </p>
+        <p>
+          If you want to collaborate on
           a project or simply want to chat, feel free to reach out to me as well!
         </p>
 
