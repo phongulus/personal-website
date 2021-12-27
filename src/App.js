b55c1f15190b69b7eb0e0312639a3ce952c16617
Phong Le <phongnguyen.le22@gmail.com>
2021-12-27 20:01:37 +0800

    Added new font, modified attributions section

--- src/App.js
+++ src/App.js
@@ -22,12 +22,13 @@ function App() {
 
       <Contact />
       
-      <p></p>
-      <div>
-        Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+      
+      <div className='attribution'>
+        <p>
+        Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik" className='attribution-link'>Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon" className='attribution-link'>www.flaticon.com</a> / 
+        Photo by <a href="https://unsplash.com/@nathananderson?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" className='attribution-link'>Nathan Anderson</a> on <a href="https://unsplash.com/s/photos/night-sky?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" className='attribution-link'>Unsplash</a>
+        </p>
       </div>
-      <p></p>
-
     </div>
   );
 }
